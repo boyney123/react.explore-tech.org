59c0b2337f80f5620e77469ad5dd4d11691ab5a7
David Boyne <davidboyne123@hotmail.co.uk>
2019-02-11 07:56:52 +0000

    Added tests for icon-to-category mapper

---
+++ src/utils/icon-to-category/spec.js
@@ -0,0 +1,27 @@
+import tutorial from '../../icons/tutorial.svg'
+import form from '../../icons/form.svg'
+import page from '../../icons/page.svg'
+import bricks from '../../icons/bricks.svg'
+import hammer from '../../icons/hammer.svg'
+import testing from '../../icons/testing.svg'
+import tools from '../../icons/tools.svg'
+import library from '../../icons/library.svg'
+import routing from '../../icons/routing.svg'
+import styles from '../../icons/styles.svg'
+import example from '../../icons/example.svg'
+
+import map from './'
+
+describe('icon-to-category', () => {
+  it('should map the correct Category to the correct icon', () => {
+    expect(map.Tutorials).toEqual(tutorial)
+    expect(map.Components).toEqual(bricks)
+    expect(map.Tools).toEqual(hammer)
+    expect(map.Styles).toEqual(styles)
+    expect(map.Boilerplates).toEqual(tools)
+    expect(map.Routing).toEqual(routing)
+    expect(map.Libraries).toEqual(library)
+    expect(map.Testing).toEqual(testing)
+    expect(map.Examples).toEqual(example)
+  })
+})
