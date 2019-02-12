61dadb7b05f4c0e81d45ca4abb146c0519aa32a5
David Boyne <davidboyne123@hotmail.co.uk>
2019-02-12 08:27:38 +0000

    Added small header link to go back

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import './styles.css'
 
 const Navigation = () => {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation" />
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <Link to="/">react.explore-tech.org</Link>
+        {/* <a class="navbar-item" href="/"> */}
+        {/* react.explore-tech.org */}
+        {/* </a> */}
+      </div>
+    </nav>
   )
 }
 
