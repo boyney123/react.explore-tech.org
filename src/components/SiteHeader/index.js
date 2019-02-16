fd5d1c573554d86d27c281e63421bc75cda0412f
David Boyne <davidboyne123@hotmail.co.uk>
2019-02-16 11:25:34 +0000

    Added share button

--- src/components/SiteHeader/index.js
+++ src/components/SiteHeader/index.js
@@ -16,6 +16,15 @@ const SiteHeader = ({ count }) => {
           <h2 className="subtitle">{description}</h2>
 
           <a
+            className="button is-medium add-action twitter"
+            href="https://twitter.com/intent/tweet?text=An%20open%20source%20project%20that%20lists%20resources%20to%20help%20developers%20explore%20React%20%F0%9F%91%8D.%0A%0A%20https%3A%2F%2Freact.explore-tech.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-twitter" />
+            Share with others
+          </a>
+          <a
             className="button is-medium add-action"
             href="https://github.com/boyney123/react.explore-tech.org/blob/master/CONTRIBUTING.md#adding-a-material-to-the-website"
             target="_blank"
